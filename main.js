530b3c0cd15bfc49bb9ef4fae9905d42823976e2
Tenet <40796508+luisadha@users.noreply.github.com>
2025-10-07 12:25:08 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // main.js
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js");
+if (navigator.serviceWorker) {
+        navigator.serviceWorker.register (
+          '/drawercli-aurora/service-worker.js',
+          {scope: '/drawercli-aurora/'}
+        )
 }
