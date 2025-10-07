c588dfc0db15743ca60622d8f8b4d71138fea4dc
Tenet <40796508+luisadha@users.noreply.github.com>
2025-10-07 13:38:20 +0700

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// main.js
-if (navigator.serviceWorker) {
-        navigator.serviceWorker.register (
-          '/drawercli-aurora/service-worker.js',
-          {scope: '/drawercli-aurora/'}
-        )
-}
