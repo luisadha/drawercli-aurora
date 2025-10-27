5c41d54e694ff798bbcd11d0fa39e0b992155383
Tenet <40796508+luisadha@users.noreply.github.com>
2025-10-27 11:33:26 +0700

    Delete assets/js/precodecopy.js

--- assets/js/precodecopy.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('pre > code').forEach(el => {
-  const btn = document.createElement('span');
-  btn.className = 'copy-button';
-  btn.onclick = () => navigator.clipboard.writeText(el.innerText);
-  el.parentNode.append(btn);
-});
